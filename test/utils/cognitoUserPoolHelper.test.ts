ec8ce8b005779b322331f7c8621ad414e8e249ac
Ebenezer Mathebula <mathebu@amazon.com>
2022-08-14 03:31:38 +0200

    Setup Mocha, and nyc

--- test/utils/cognitoUserPoolHelper.test.ts
+++ test/utils/cognitoUserPoolHelper.test.ts
@@ -1,8 +1,9 @@
 import {expect} from 'chai';
+import {describe} from 'mocha';
 
 describe('CognitoUserPoolHelper', () => {
     describe('signUp', () => {
-        test('pass test', () => {
+        it('pass test', () => {
             expect(1 + 2).to.deep.equal(3);
         });
     });
